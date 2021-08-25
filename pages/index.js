0a71c2893d90f39a27fe89b51d6582be893c3fd6
divit-agrawal <73773075+divit-agrawal@users.noreply.github.com>
2021-08-25 22:29:09 +0530

    lol

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import homeStyles from "../styles/Home.module.css";
 /*Components*/
 import Landing from "../components/Landing";
 import About from "../components/About";
+import Events from "../components/Events";
 
 export default function Home() {
   return (
     <div className={homeStyles.container}>
       <Landing />
       <About />
+      <Events />
     </div>
   );
 }
