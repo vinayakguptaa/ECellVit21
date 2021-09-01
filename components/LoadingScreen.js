01f990b2b2f1e8f0271d851263f176c0d56ccaa1
vansh chinda <vanshchinda@gmail.com>
2021-09-01 20:48:43 +0530

    commented loading screen

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,11 +1,11 @@
-import loadingScreenStyles from "../styles/LoadingScreen.module.css";
+// import loadingScreenStyles from "../styles/LoadingScreen.module.css";
 
-export default function LoadingScreen(props) {
-  return (
-    <div className={loadingScreenStyles.mainContainer}>
-      <div className={loadingScreenStyles.circ}></div>
-      <div className={loadingScreenStyles.circ}></div>
-      <div className={loadingScreenStyles.circ}></div>
-    </div>
-  );
-}
+// export default function LoadingScreen(props) {
+//   return (
+//     <div className={loadingScreenStyles.mainContainer}>
+//       <div className={loadingScreenStyles.circ}></div>
+//       <div className={loadingScreenStyles.circ}></div>
+//       <div className={loadingScreenStyles.circ}></div>
+//     </div>
+//   );
+// }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Head from "next/head";
 import homeStyles from "../styles/Home.module.css";
 
 /*Components*/
-import LoadingScreen from "../components/LoadingScreen";
+// import LoadingScreen from "../components/LoadingScreen";
 import Landing from "../components/Landing";
 import About from "../components/About";
 import Events from "../components/Events";
@@ -14,15 +14,15 @@ import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  const [loading, setloading] = useState(true);
+  // const [loading, setloading] = useState(true);
   const [title, setTitle] = useState("ENTREPRENEURSHIP CELL VIT");
   const [navActive, setNavActive] = useState(true);
 
   useEffect(() => {
-    setloading(true);
-    setTimeout(() => {
-      setloading(false);
-    }, 8000);
+    // setloading(true);
+    // setTimeout(() => {
+    //   setloading(false);
+    // }, 8000);
 
     addEventListener("resize", () => {
       if (window.innerWidth < 1000) {
