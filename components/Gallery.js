6605fbcc851a12dc64f635b179057d40b0ad7d67
vansh chinda <vanshchinda@gmail.com>
2021-09-03 16:56:48 +0530

    gallery updated

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,98 +1,66 @@
-import React, { useState, useEffect, useCallback } from "react";
-
-import { useEmblaCarousel } from "embla-carousel/react";
+import { useState, useCallback } from "react";
 
 import Image from "next/image";
 
-import { useRecursiveTimeout } from "./useRecursiveTimeout";
-import { PrevButton, NextButton } from "./EmblaCarouselButtons";
+import galleryStyles from "../styles/Gallery.module.css";
 
 import { mediaByIndex } from "../public/galleryImages/index.js";
 
-import galleryStyles from "../styles/Gallery.module.css";
-
-const AUTOPLAY_INTERVAL = 4000;
+import ImageViewer from "react-simple-image-viewer";
 
 export default function Gallery() {
-  const SLIDE_COUNT = 6;
-  const slides = Array.from(Array(SLIDE_COUNT).keys());
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const images = [
+    mediaByIndex(1),
+    mediaByIndex(2),
+    mediaByIndex(3),
+    mediaByIndex(4),
+    mediaByIndex(5),
+  ];
 
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
 
-  const [viewportRef, embla] = useEmblaCarousel({ skipSnaps: false });
-  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
-  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
-
-  const autoplay = useCallback(() => {
-    if (!embla) return;
-    if (embla.canScrollNext()) {
-      embla.scrollNext();
-    } else {
-      embla.scrollTo(0);
-    }
-  }, [embla]);
-
-  const { play, stop } = useRecursiveTimeout(autoplay, AUTOPLAY_INTERVAL);
-
-  const scrollNext = useCallback(() => {
-    if (!embla) return;
-    embla.scrollNext();
-    stop();
-  }, [embla, stop]);
-
-  const scrollPrev = useCallback(() => {
-    if (!embla) return;
-    embla.scrollPrev();
-    stop();
-  }, [embla, stop]);
-
-  const onSelect = useCallback(() => {
-    if (!embla) return;
-    setPrevBtnEnabled(embla.canScrollPrev());
-    setNextBtnEnabled(embla.canScrollNext());
-  }, [embla]);
-
-  useEffect(() => {
-    if (!embla) return;
-    onSelect();
-    embla.on("select", onSelect);
-    embla.on("pointerDown", stop);
-  }, [embla, onSelect, stop]);
-
-  useEffect(() => {
-    play();
-  }, [play]);
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
 
   return (
     <div className={galleryStyles.mainContainer} id="gallery">
       <div className={galleryStyles.title}>
         <a href="#home">Gallery</a>
-        <div className={galleryStyles.embla__slide__inner}></div>
       </div>
       <div className={galleryStyles.container}>
-        <div className={galleryStyles.embla}>
-          <div className={galleryStyles.embla__viewport} ref={viewportRef}>
-            <div className={galleryStyles.embla__container}>
-              {slides.map((index) => (
-                <div className={galleryStyles.embla__slide} key={index}>
-                  <div className={galleryStyles.embla__slide__inner}>
-                    <Image
-                      className={galleryStyles.embla__slide__img}
-                      src={mediaByIndex(index)}
-                      alt="Gallery"
-                      layout="fill"
-                      loading="eager"
-                      // placeholder="blur"
-                      // blurDataURL={mediaByIndex(index)}
-                    />
-                  </div>
-                </div>
-              ))}
+        <div className={galleryStyles.imageMainContainer}>
+          {images.map((src, index) => (
+            <div className={galleryStyles.imageItemContainer}>
+              <Image
+                src={src}
+                onClick={() => openImageViewer(index)}
+                width="384px"
+                height="216px"
+                key={index}
+                alt=""
+                // layout="fill"
+                className={galleryStyles.imageItem}
+              />
             </div>
-          </div>
+          ))}
         </div>
-        <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
-        <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
+
+        {isViewerOpen && (
+          <ImageViewer
+            src={images}
+            currentIndex={currentImage}
+            disableScroll={false}
+            closeOnClickOutside={true}
+            onClose={closeImageViewer}
+          />
+        )}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Head from "next/head";
 import homeStyles from "../styles/Home.module.css";
 
 /*Components*/
-// import LoadingScreen from "../components/LoadingScreen";
 import Landing from "../components/Landing";
 import About from "../components/About";
 import Events from "../components/Events";
@@ -14,16 +13,10 @@ import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  // const [loading, setloading] = useState(true);
   const [title, setTitle] = useState("ENTREPRENEURSHIP CELL VIT");
   const [navActive, setNavActive] = useState(true);
 
   useEffect(() => {
-    // setloading(true);
-    // setTimeout(() => {
-    //   setloading(false);
-    // }, 8000);
-
     addEventListener("resize", () => {
       if (window.innerWidth < 1000) {
         setTitle("E-CELL");
@@ -71,9 +64,6 @@ export default function Home() {
         <title>Entrepreneurship Cell VIT</title>
       </Head>
 
-      {/* {loading ? (
-        <LoadingScreen loading={loading} />
-      ) : ( */}
       <div className={homeStyles.container}>
         <Landing title={title} navActive={navActive} />
         <About />
@@ -82,7 +72,6 @@ export default function Home() {
         <Gallery />
         <Footer />
       </div>
-      {/* )} */}
     </>
   );
 }
